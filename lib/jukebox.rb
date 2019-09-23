a760dd336d11b43885daf978fafe9e413e4b1933
Peter Werner <pcw1995@yahoo.com>
2019-09-23 16:42:31 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -7,8 +7,7 @@ songs = [
   "The Cults - Abducted",
   "Phoenix - Consolation Prizes",
   "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
+  "Amos Lee - Keep It Loose, Keep It Tight"]
 def help 
   puts "I accept the following commands:"
   puts "- help : displays this help message"
@@ -30,15 +29,15 @@ def play(songs)
   puts "Please enter a song name or number: "
   input = gets.strip
   i = 0
-  found = 0 
-  p input
+  found = 0
   while songs.length>i do 
     num = "#{i+1}" 
     if (input == songs[i] or input == num) 
       puts "Playing #{songs[i]}"
       found=1
+    else 
+      p "Invalid input, please try again"
     end
-    
     i +=1 
   end 
   if found == 0 
@@ -51,29 +50,31 @@ def exit_jukebox
 end 
 
 def run(argument)
+  songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"]
   puts "Please enter a command:"
   input = gets.strip
-  if input == "exit"
-    exit_jukebox
-  end 
-  while input != "exit" do 
-    if input == list
+  while input != "exit" do
+    if input == "list"
       list(songs)
     elsif input == "play" 
       play(songs)
     elsif input == "help"
-      help 
-    end 
-  end 
-  
+      help
+    end
+    puts "Please enter a command:"
+    input = gets.strip
+  end
+  if input == "exit"
+    exit_jukebox
+  end
 end 
-
-
-
-
-
-
-
-
-
 
