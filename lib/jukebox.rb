ea9e821b437eac8e4463a77c4eae7edc7ba38cae
Peter Werner <pcw1995@yahoo.com>
2019-09-23 16:40:38 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -51,6 +51,16 @@ def exit_jukebox
 end 
 
 def run(argument)
+  songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"]
   puts "Please enter a command:"
   input = gets.strip
   while input != "exit" do
